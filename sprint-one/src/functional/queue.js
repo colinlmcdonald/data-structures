f1dcf660505cb4c36e383eed9226fbfcf9c27fc1
colinlmcdonald <mickydwow@gmail.com>
2016-01-26 11:55:49 -0800

    Working on da queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,42 @@
 var Queue = function(){
+  var count = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  var storage = {
+    oldest: 0,
+    newest: 0
+  };
   // Implement the methods below
-
   someInstance.enqueue = function(value){
+    someInstance[storage.newest] = value;
+    storage.newest++;
   };
 
   someInstance.dequeue = function(){
+    //first in, first out
+    //enqueue adds to the back, dequeue takes from the front
+    //enqueue is the newest, dequeue is the oldest
+    //return oldest of two values && return oldest value after new ones have been added
+    //how do i find the oldest value in an object?
+    if (storage.newest) {
+      storage.newest--;
+    }
+    return someInstance[storage.oldest];
   };
 
   someInstance.size = function(){
+    return (storage.newest - storage.oldest);
   };
-
   return someInstance;
 };
+
+
+someInstance = {
+  0: a,
+  1: b,
+  2: c
+}
+
+oldest: 0
+newest: 3
