7b3bfb67c0304407fede8c892cd52eb060cd66b0
colinlmcdonald <mickydwow@gmail.com>
2016-01-27 16:33:46 -0800

    Finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,37 @@
 var LinkedList = function(){
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = {};
+  list.tail = {};
+  list.tail.storage = [];
+
 
   list.addToTail = function(value){
+    this.head.value = this.tail.value;
+    this.tail.value = value;
+    list.tail.storage.push(value);
   };
 
   list.removeHead = function(){
+    var tail = this.tail.storage;
+    for (var i = 0; i < tail.length; i++) {
+      if (tail[i] = this.head.value) {
+        tail.splice(i);
+      }
+    }
+    delete this.head.value;
+    this.head.value = this.tail.value;
+    return this.head.value;
   };
 
   list.contains = function(target){
+    var tail = this.tail.storage;
+    var itIsThere = false;
+    for (var i = 0; i < tail.length; i++) {
+      if (tail[i] === target) {
+        itIsThere = true;
+      } 
+    }
+    return itIsThere;
   };
 
   return list;
